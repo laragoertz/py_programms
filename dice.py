9265a729df554e995da18933c497110c704db7ec
Lara <58141493+laragoertz@users.noreply.github.com>
2021-02-08 22:16:25 +0100

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,31 @@
+# roll the dice
+from random import randint 
+
+print('roll the dice')
+roll=(randint(1,7))
+print(roll)
+print(' _______')
+if roll ==1:
+    print('|       |\n|   o   |\n|       |')
+elif roll ==2:
+    print('|      o|\n|       |')
+    print('|o      |')
+elif roll ==3:
+    print('|      o|')
+    print('|   o   |')
+    print('|o      |')
+elif roll ==4:
+    print('|o     o|\n|       |')
+    print('|o     o|')
+elif roll ==5:
+    print('|o     o|')
+    print('|   o   |')
+    print('|o     o|')
+elif roll ==6:
+    print('|o     o|')
+    print('|o     o|')
+    print('|o     o|')
+else: 
+    roll ==7
+    print('Another roll is not possible!')
+print(' _______\n\n')
